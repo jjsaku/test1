dc423d0a7de018d562944f8682bb4bc931cf824b
jjsaku <jjsaku123@gmail.com>
2023-06-19 22:19:30 +0800

    models.py

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from mysite.models import Post
+from mysite.models import NewTable
+from mysite.models import Product
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(NewTable)
+admin.site.register(Product)
